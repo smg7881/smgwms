d4801d7cd34d6895f3cc9cbec29e00d6cb799968
송명근 <smg10djr@naver.com>
2026-03-03 18:58:32 +0900

    이름 변경함

--- config/initializers/zeitwerk_relocated_models.rb
+++ config/initializers/zeitwerk_relocated_models.rb
@@ -1,18 +1,19 @@
-std_and_system_model_dirs = [
+relocated_model_dirs = [
   Rails.root.join("app/models/std"),
-  Rails.root.join("app/models/system")
+  Rails.root.join("app/models/system"),
+  Rails.root.join("app/models/om")
 ]
 
-prefixed_model_files = Dir.glob(Rails.root.join("app/models/om/om_*.rb")).map { |path| Pathname.new(path) } +
-  Dir.glob(Rails.root.join("app/models/wm/wm_*.rb")).map { |path| Pathname.new(path) }
+wm_mismatched_model_files = %w[area location workplace zone].map do |name|
+  Rails.root.join("app/models/wm/#{name}.rb")
+end
 
-Rails.autoloaders.main.ignore(*(std_and_system_model_dirs + prefixed_model_files))
+Rails.autoloaders.main.ignore(*(relocated_model_dirs + wm_mismatched_model_files))
 
 Rails.application.config.to_prepare do
-  relocated_model_files = Dir.glob(Rails.root.join("app/models/std/std_*.rb")) +
-    Dir.glob(Rails.root.join("app/models/system/adm_*.rb")) +
-    Dir.glob(Rails.root.join("app/models/om/om_*.rb")) +
-    Dir.glob(Rails.root.join("app/models/wm/wm_*.rb"))
+  relocated_model_files = relocated_model_dirs.flat_map do |dir|
+    Dir.glob(dir.join("*.rb"))
+  end + wm_mismatched_model_files.map(&:to_s)
 
   relocated_model_files.sort.each do |file_path|
     require_dependency file_path
